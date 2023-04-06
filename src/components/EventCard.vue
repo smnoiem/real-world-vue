c6915fd40727eb27677769919c52db7ff5b79712
Noiem Uddin <smnoiem@gmail.com>
2023-04-06 23:37:09 +0600

    slots and api calls with axios

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,32 +1,25 @@
 <template>
   <router-link
     class="event-link"
-    :to="{ name: 'event-show', params: { id: '1' } }"
+    :to="{ name: 'event-show', params: { id: event.id } }"
   >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users" />
-      <span>{{ event.attendees.length }} attending </span>
+      <BaseIcon name="users">{{ event.attendees.length }} attending </BaseIcon>
     </div>
   </router-link>
 </template>
 
 <script>
 export default {
+  props: {
+    event: {
+      type: Object,
+    },
+  },
   data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Beach Cleanup',
-        date: 'Tues Aug 19, 2018',
-        time: '6:00',
-        attendees: [
-          { id: 'abc123', name: 'John Doe' },
-          { id: 'def456', name: 'Jane Marry' },
-        ],
-      },
-    }
+    return {}
   },
 }
 </script>

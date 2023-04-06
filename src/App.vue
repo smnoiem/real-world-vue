a7cd32f37337ccbbd3e4bf8a05875eda4575aa24
Noiem Uddin <smnoiem@gmail.com>
2023-04-06 13:44:29 +0600

    dynamic routes and route practices

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <nav>
       <router-link :to="{ name: 'event-list' }">List</router-link> |
-      <router-link :to="{ name: 'event-show' }">Show Event #1</router-link> |
       <router-link :to="{ name: 'event-create' }">Create</router-link>
     </nav>
     <router-view />
